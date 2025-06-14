b2b71e83e048ed5ae23b9e203c80d8dfbe3bbd83
manikandan <manikandan110305@gmail.com>
2025-06-15 01:42:54 +0530

    Update for new repo

--- src/constant/Url.jsx
+++ src/constant/Url.jsx
@@ -1,3 +1,5 @@
-const BaseUrl = 'http://localhost:5000'
+const BaseUrl = import.meta.env.PROD
+  ? "https://manikandan782692.github.io" // ✅ Replace with your actual deployed backend URL (Heroku/Render)
+  : "http://localhost:5000";
 
 export default BaseUrl;
