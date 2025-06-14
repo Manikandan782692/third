65d55232060429ff4a7275995e4ad182b7f6e399
manikandan <manikandan110305@gmail.com>
2025-06-15 02:23:35 +0530

    Update for new repo

--- src/constant/Url.jsx
+++ src/constant/Url.jsx
@@ -1,5 +1,5 @@
 const BaseUrl = import.meta.env.PROD
-  ? "https://<xcloneweb>.onrender.com" // replace with your actual Render backend URL
+  ? "https://third-1-uchf.onrender.com" // replace with your actual Render backend URL
   : "http://localhost:5000";
 
 
